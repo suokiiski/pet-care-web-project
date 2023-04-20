6bce355d8a7be149faeb4604906bcae1c37df4cf
fngnv <finogenova.vera4@gmail.com>
2023-04-20 13:50:35 +0300

    Lisää sisältöa footeriin

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 const Footer = () => {
     const year = new Date().getFullYear();
@@ -9,36 +10,54 @@ const Footer = () => {
             <img className='logoFooter' src="./images/logo-mini.png" alt="logo"/>
             <div className='container someIconsContainer'>
                 <div className='row'>
-                    <div className='col-sm-2 someIcon'>
-                        <SocialIcon url='https://twitter.com' />
+                    <div className='col-sm-2'>
+                        <ul className='ulFooter '>
+                            <li className='someIcon'>
+                                <SocialIcon url="https://twitter.com" />
+                            </li>
+                            <li className='someIcon'>
+                                <SocialIcon url="https://facebook.com" />
+                            </li>
+                            <li className='someIcon'>
+                                <SocialIcon url="https://instagram.com" />
+                            </li>
+                            <li className='someIcon'>
+                                <SocialIcon url="https://github.com/suokiiski/pet-care-web-project/tree/master" />
+                            </li>
+                        </ul>
                     </div>
+
                     <div className='col-sm-3'>
-                        <h5 className='footerH5'>Support</h5>
+                        <h6 className='h6Footer'>Contact us</h6>
+                        <ul className='ulFooter'>
+                            <li>
+                                Helsinki, Myllypuro
+                            </li>
+                            <li>+358 666 13 13</li>
+                            <li>feedback@petnanny.com</li>
+                        </ul>
                     </div>
+
                     <div className='col-sm-3'>
-                        <h5 className='footerH5'>Company</h5>
+                        <h6 className='h6Footer'>Company</h6>
+                        <ul className='ulFooter'>
+                            <li>History</li>
+                            <li>Blog</li>
+                            <li>About Us</li>
+                        </ul>
                     </div>
+
                     <div className='col-sm-3'>
-                        <h5 className='footerH5'>Terms & Policies</h5>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-sm-2 someIcon'>
-                        <SocialIcon url='https://www.instagram.com' />
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-sm-2 someIcon'>
-                        <SocialIcon url='https://www.facebook.com' />
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-sm-2 someIcon'>
-                        <SocialIcon url='https://github.com/suokiiski/pet-care-web-project/tree/master' />
+                        <h6 className='h6Footer'>Terms & Policy</h6>
+                        <ul className='ulFooter'>
+                            <li>Policies</li>
+                            <li>Terms of use</li>
+                            <li>Privacy</li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <h5>Copyright © Pet Nanny {year}</h5>
+            <p>Copyright © Pet Nanny {year}</p>
         </footer>
     )
 }
