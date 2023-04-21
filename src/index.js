a99a249557cbac14289531859290454a71306a70
suokiiski <kirsiito@metropolia.fi>
2023-04-21 14:15:30 +0300

    Header toimii

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
 import reportWebVitals from './reportWebVitals';
 
 //bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
