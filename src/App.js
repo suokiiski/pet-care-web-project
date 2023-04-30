bb2b44ec581c9c991480369c0d248f1b7ee71b0d
iromjalis <iromjalis@gmail.com>
2023-05-01 01:34:42 +0300

    routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
 //components
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Header from "./components/header";
-import Footer from './components/footer'
-import RegistrationForm from "./components/registrationForm";
+import Footer from "./components/footer";
+// import RegistrationForm from "./components/registrationForm";
 import Login from "./components/login";
-import posterService from './services/posters'
-import logo from './logo.svg';
+import posterService from "./services/posters";
 //styles
-import './App.css';
+import "./App.css";
 
-import { useState, useEffect } from 'react';
-
-
+import { useState, useEffect } from "react";
 
 function App() {
-    const[data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        posterService
-            .getAll()
-            .then((resp) => {
-                setData(resp.data);
-            });
-    }, [])
-
+  useEffect(() => {
+    posterService.getAll().then((resp) => {
+      setData(resp.data);
+    });
+  }, []);
 
   const render = (newObj) => {
     console.log(newObj);
     setData([...newObj, ...data]);
   };
-  
+
   return (
     <div className="App">
       <Router>
-          <Header />
-          <Routes>
-            <Route exact path='/' element={<Main data={data} addToRender={render} />} />>;
-              <Route path='/sign-in' element={<Login />} />
-          </Routes>
+        <Header />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Main data={data} addToRender={render} />}
+          />
+          <Route path="/sign-in" element={<Login />} />
+        </Routes>
       </Router>
-        <Footer />
+      <Footer />
     </div>
   );
 }
