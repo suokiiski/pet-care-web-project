712859d6ffbe49685d8383b5f8460edf5eaf8e6e
iromjalis <iromjalis@gmail.com>
2023-05-01 01:37:55 +0300

    edit button and favorite button working fine

--- src/components/Article.js
+++ src/components/Article.js
@@ -14,8 +14,9 @@ const Article = ({
   omistaja,
   cat,
   deleteArticle,
+  handleOpen,
+  saveOnFavoutires,
 }) => {
-
   return (
     <article className="article" id={id}>
       <figure>
@@ -48,7 +49,7 @@ const Article = ({
             className="edit_btn edit"
             title="you can edit article (opening modal window)"
           >
-            <button>&#9998;</button>
+            <button onClick={() => handleOpen(id)}>&#9998;</button>
           </li>
           <li className="edit_btn bin" title="delete the whole article">
             <button name="delete" onClick={() => deleteArticle(id)}>
@@ -56,7 +57,7 @@ const Article = ({
             </button>
           </li>
           <li className="edit_btn star" title="save for the future">
-            <button>⭐</button>
+            <button onClick={() => saveOnFavoutires(id)}>⭐</button>
           </li>
         </ul>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,33 +2,74 @@ import React, { Component } from "react";
 import ArticleList from "./ArticleList";
 import Modal from "./Modal";
 import AddNewArticleForm from "./AddNewArticleForm";
+import { ToastContainer, toast } from "react-toastify";
+
 //styles
 import "../styles/Main.css";
 import "../styles/scrollbar.css";
 import shortid from "shortid";
 import posterService from "../services/posters";
 
+//for toTop button on the right bottom corner
+const debounce = require("lodash.debounce");
+
+window.onscroll = debounce(scrollFunction, 250);
+
+function scrollFunction() {
+  const myButton = document.getElementById("myBtn");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    myButton.style.display = "block";
+    myButton.classList.add("animate__bounceInDown");
+    myButton.classList.remove("animate__bounceOutUp");
+  } else {
+    myButton.style.display = "none";
+    myButton.classList.remove("animate__bounceInDown");
+  }
+}
+
+function onScrollToTop() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+function onScrollToBottom() {
+  window.scrollTo({
+    bottom: 0,
+    right: 0,
+    behavior: "smooth",
+  });
+}
+
 class Main extends Component {
   componentDidUpdate(prevProps) {
     posterService.getAll().then((resp) => {
-      this.setState(({ articles }) => ({
+      this.setState(() => ({
         articles: resp.data,
       }));
     });
   }
   state = {
+    id: "",
     login: false,
     showModal: false,
     articles: this.props.data,
+    editModalOpen: false,
+    favourites: [],
   };
 
   toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+    this.setState(({ editModalOpen }) => ({
+      showModal: !editModalOpen,
+    }));
   };
 
   //poista username jos ei toimi
   addNewArticle = (data) => {
-    const { articles } = this.state;
     const { name, tel, text, cat, img, omistaja, username } = data;
     const article = {
       id: shortid.generate(),
@@ -38,14 +79,43 @@ class Main extends Component {
       cat,
       src: img,
       omistaja,
-      username
+      username,
     };
 
     this.setState(({ articles }) => ({
       articles: [...articles, this.props.data],
+      showModal: false,
+      editModalOpen: false,
     }));
 
+    onScrollToBottom();
+
     posterService.create(article);
+  };
+
+  editArticle = (data) => {
+    const { name, tel, text, cat, img, omistaja, username } = data;
+    const id = this.state.id;
+    const newArticle = {
+      id,
+      nimi: name,
+      tel,
+      text,
+      cat,
+      src: img,
+      omistaja,
+      username,
+    };
+
+    posterService.update(id, newArticle);
+
+    this.setState(() => ({
+      editModalOpen: false,
+      showModal: false,
+      articles: this.props.data.map((item) =>
+        item.id === id ? newArticle : item
+      ),
+    }));
   };
 
   deleteArticle = (articleId) => {
@@ -55,37 +125,33 @@ class Main extends Component {
       }));
 
       posterService.remove(articleId);
+
+      onScrollToTop();
     } else {
       return;
     }
+  };
 
-    // posters.deleteById(articleId);
-    // console.log(article);
+  handleOpenEditModal = (id) => {
+    this.setState(() => ({
+      editModalOpen: true,
+      showModal: false,
+      id,
+    }));
+  };
+
+  saveOnFavoutires = (articleId) => {
+    alert("Saved successfully!");
+
+    this.setState(({ favourites }) => ({
+      editModalOpen: false,
+      showModal: false,
+      favourites: [...favourites, articleId],
+    }));
   };
 
   render() {
-    const { showModal } = this.state;
-
-    // console.log(posters);
-    if(localStorage.getItem('status') === null) {
-      return (
-          <div className="main_container">
-            {showModal && (
-                <Modal onClose={this.toggleModal}>
-                  <button onClick={this.toggleModal} className="close-btn">
-                    X
-                  </button>
-                  <AddNewArticleForm
-                      onClose={this.toggleModal}
-                      onSubmit={this.addNewArticle}
-                  />
-                </Modal>
-            )}
-            <ArticleList items={this.props.data} />
-          </div>
-      )
-    }
-
+    const { showModal, editModalOpen } = this.state;
     return (
       <div className="main_container">
         {showModal && (
@@ -106,7 +172,29 @@ class Main extends Component {
         <ArticleList
           items={this.state.articles}
           deleteArticle={this.deleteArticle}
+          handleOpen={this.handleOpenEditModal}
+          saveOnFavoutires={this.saveOnFavoutires}
         />
+        {editModalOpen && (
+          <Modal onClose={this.toggleModal}>
+            <button onClick={this.toggleModal} className="close-btn">
+              X
+            </button>
+            <AddNewArticleForm
+              onClose={this.toggleModal}
+              onSubmit={this.editArticle}
+            />
+          </Modal>
+        )}
+
+        <span
+          class="up-button animate__animated"
+          id="myBtn"
+          rel="noopener noreferrer"
+          onClick={onScrollToTop}
+        >
+          &#11165;
+        </span>
       </div>
     );
   }
